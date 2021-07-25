a9fc020d366c015b9fc3907327c247c48f6d2005
eytantirosh <eytantirosh@mail.tau.ac.il>
2021-07-25 17:57:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 var suits = ["spades", "diamonds", "clubs", "hearts"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+var valuesToNumbers = {"A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7
+"8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10};
+
+var LIMIT = 21;
+
 function getDeck()
 {
     var deck = new Array();
@@ -54,5 +59,59 @@ function renderDeck(deck)
         card.className = 'card';
     document.getElementById("deck").appendChild(card);
     }
+}
+
+function openTop(deck)
+//opens the top card of the deck
+//TODO add visual effect
+{
+    if (deck.length <= 0){
+        //print that the deck is empty
+        return //what to return?
+    }
+    return deck.pop();
+}
+
+function checkLimit(count){
+
+}
+
+function userDecision()
+//TODO ask the user to "hit" or to "stand"
+{
+
+}
+
+function userTurn(deck)
+{
+    var openedCards = new Array();
+    var count = 0;
+    var card = openTop(deck);
+    openedCards.push(card);
+    card = openTop(deck);
+    openedCards.push(card);
+    var continuePlay = "hit";
+    while (continuePlay != "stand"){
+        card = openTop(deck);
+        openedCards.push(card);
+        count = count + valuesToNumbers(card[Value]) //is it OK?
+
+    }
+}
+
+function singleGame()
+{
+    //TODO get the amount of betting money of the user
+
+    var deck = getDeck();
+    deck = shuffle(deck);
+    var under
+    userTurn
+
+}
+
+function gameDuration()
+{
+
 }
 
