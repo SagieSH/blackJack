e010cbca40f0433e8e2f12151a72f79bfab715f9
SagieSH <sagie.shlfman@gmail.com>
2021-07-26 16:26:53 +0300

    26.07 16:26

--- main.js
+++ main.js
@@ -1,70 +1,77 @@
 
 var suits = ["spades", "diamonds", "clubs", "hearts"];
+// var suits = ["♠", "♦", "♣", "♥"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
-var suitsToChars = {"spades": "S", "diamonds": "D", "clubs": "C", "hearts": "H"};
 var valuesToNumbers = {"A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
 "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10};
 
-var LIMIT = 21;
+var userToIndex = {"Player": 0, "Dealer": 1};
+
+var deck = new Array();
+var playerTurn = false;     // indicates whether the player can hit or stand
+var inGame = false;         // indicates whether the player is currently in game
+var BUSTLIMIT = 22;
+var DEALLIMIT = 17;
+
+var indexInTable = [0, 0];
+var amount = [0, 0];
+var countAces = 0;
 
 
-function beforeGame() 
-{
+function beforeGame() {
     setBalance(100);
+    document.getElementById("msg").innerText = "Please start a new game"
 }
 
-function setBalance(balance)
-{
+
+function setBalance(balance) {
     document.getElementById("balance").innerText = "Balance: " + balance;
 }
 
-function setAmount(user, amount) 
-{
-    id = "totalPlayer";
-    if (user == "Dealer") {
-        id = "totalDealer";
-    }
-    document.getElementById(id).innerText = user + " total: " + amount;
+
+function setAmount(user, newAmount) {
+    amount[userToIndex[user]] = newAmount;
+    var id = "total" + user;
+    document.getElementById(id).innerText = user + " total: " + newAmount;
 }
 
 
-function changeHand(user, index, card) {
+function addAmount(user, adding) {
+    ret = amount[userToIndex[user]] + adding;
+    setAmount(user, ret);
+}
+
+
+function placeCard(user, card) {
+    // place the card in the index of the user's table
+    index = indexInTable[userToIndex[user]];
     if (index < 1 || index > 7) {
         console.log("bad");
     }
     var u = "p";
-    if (user == "Dealer"){
+    if (user == "Dealer") {
         u = "d"
     }
-    document.getElementById(u + index).innerText = card[Suit] + "\n" + card[Value]; // is it OK?
-    setAmount(user, valuesToNumbers[card[Value]]);
+    document.getElementById(u + index).innerText = card["suit"] + "\n" + card["value"];
+    addAmount(user, valuesToNumbers[card["value"]]);
+    indexInTable[userToIndex[user]]++;
 }
 
 
-
-
-
-
-
-function getDeck()
-{
-    var deck = new Array();
+function getDeck() {
 
     for(var i = 0; i < suits.length; i++)
     {
         for(var x = 0; x < values.length; x++)
         {
-            var card = {Value: values[x], Suit: suits[i]};
+            var card = {"value": values[x], "suit": suits[i]};
             deck.push(card);
         }
     }
-
-    return deck;
 }
 
-function shuffle(deck)
-{
+
+function shuffle() {
     // for 1000 turns
     // switch the values of two random cards
     for (var i = 0; i < 1000; i++)
@@ -78,127 +85,165 @@ function shuffle(deck)
     }
 }
 
-function renderDeck(deck)
-{
-    document.getElementById('deck').innerHTML = '';
 
-    for(var i = 0; i < deck.length; i++)
-    {
-        var card = document.createElement("div");
-        var icon = '';
-        if (deck[i].Suit == 'hearts')
-        icon='?';
-        else if (deck[i].Suit == 'spades')
-        icon = '?';
-        else if (deck[i].Suit == 'diamonds')
-        icon = '?';
-        else
-        icon = '?';
-
-        card.innerHTML = deck[i].Value + '' + icon;
-        card.className = 'card';
-    document.getElementById("deck").appendChild(card);
-    }
-}
-
-var index = 0;
-var countAces = 0;
-var PlayerCount = 0;
-var DealerCount = 0;
-
-function hit(deck, user, amount)
-//opens the top card of the deck
-{
-    if (deck.length <= 0){
-        console.log("empty deck");
-    }
-    index++;
-    var card = deck.pop();
-    changeHand(user, index, card);
-    if (card[Value] == "A"){
-        countAces++;
-    }
-    amount = amount + valuesToNumbers[card[Value]] //is it OK?
-
-    return checkLimit(amount);
-}
-
-function checkLimit(amount){
-    while (amount > LIMIT){
-        if (countAces == 0){
-            return 0;
+function checkIfPlayerTurn() {
+    if (!playerTurn) {
+        if (!inGame) {
+            alert("Press \'New Game\' to start!");
+        } else {
+            alert("It is not your turn!");
         }
-        //wait 1 second
-        countAces--;
-
+        return false;
     }
+    return true;
 }
 
-function playerDecision()
-//TODO ask the user to "hit" or to "stand" and returns it
-{
 
-}
-
-function changeAmount(user, amount, adding)
-{
-    var ret = amount + adding;
-    setAmount(user, ret);
-    return ret;
-}
-
-function userTurn(deck, user)
-{
-    var openedCards = new Array();
-    var countAces = 0;
-    var amount = 0;
-
-    amount = hit(deck, user, amount);
-
-    amount = hit(deck, user, amount);
-
-    if (user == "Player"){
-        var continuePlay = playerDecision();
-        while (continuePlay != "stand" || inGame == 1){
-            card = hit(deck, user);
-            openedCards.push(card);
-            count = count + valuesToNumbers[card[Value]] //is it OK?
-        }
+function hitHTML() {
+    if (!checkIfPlayerTurn()) {
+        return;
     }
-
-    else if (user == "Dealer"){
-        while (inGame == 1){
-            card = hit(deck, user);
-            openedCards.push(card);
-            count = count + valuesToNumbers[card[Value]] //is it OK?
-        }
-    }
-}
-
-function clean()
-{
-    //TODO clean: index, all the cards, countAces, 
-}
-
-function endGame()
-{
-
-}
-
-function singleGame()
-{
-    //TODO get the amount of betting money of the user
-
-    var deck = getDeck();
-    deck = shuffle(deck);
-    var succeed = userTurn(deck, "Player");
-    if (succeed == 0){
+    if (hitJS("Player")) {
+        document.getElementById("msg").innerText = "Player bust. DEALER WINS!";
         endGame();
     }
 }
 
-function gameDuration()
-{
 
+function hitJS(user) {
+//opens the top card of the deck
+    if (deck.length <= 0){
+        console.log("empty deck");
+    }
+    var card = deck.pop();
+    placeCard(user, card);
+    if (card["value"] == "A") {
+        countAces++;
+    }
+
+    return isAbove(user, BUSTLIMIT);
+
+}
+
+
+function standHTML() {
+    if (!checkIfPlayerTurn()) {
+        return;
+    }
+    playerTurn = false;
+    document.getElementById("msg").innerText = "Now wait for the dealer to finish...";
+    runDealer();
+}
+
+
+function runDealer() {
+    while (!isAbove("Dealer", DEALLIMIT)) {
+        hitJS("Dealer");
+    }
+
+    if (isAbove("Dealer", BUSTLIMIT)) {
+        document.getElementById("msg").innerText = "Dealer bust. PLAYER WINS!";
+        return;
+    }
+    playerAmount = amount[userToIndex["Player"]];
+    dealerAmount = amount[userToIndex["Dealer"]];
+
+    if (playerAmount == dealerAmount) {
+        document.getElementById("msg").innerText = "It's a tie!";
+        return;
+    }
+
+    if (playerAmount > dealerAmount) {
+        document.getElementById("msg").innerText = "PLAYER WINS!";
+        return;
+    }
+
+    document.getElementById("msg").innerText = "DEALER WINS!";
+}
+
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+
+function isAbove(user, limit) {
+    // returns whether we have passed the limit
+
+    // while (amount > LIMIT){
+    //     if (countAces == 0) {
+    //         return 0;
+    //     }
+    //     //wait 1 second
+    //     countAces--;
+
+    // }
+
+
+    return (amount[userToIndex[user]] >= limit);
+}
+
+
+function userTurn(deck, user) {
+    var countAces = 0;
+    // var amount = 0;
+
+    amount = hitJS(user, amount);
+    amount = hitJS(user, amount);
+
+    if (user == "Player"){
+        var continuePlay = playerDecision();
+        while (continuePlay != "stand" || inGameVar == 1){
+            card = hitJS(user);
+            openedCards.push(card);
+            count = count + valuesToNumbers[card["value"]] //is it OK?
+        }
+    }
+
+    else if (user == "Dealer"){
+        while (inGameVar == 1){
+            card = hitJS(user);
+            openedCards.push(card);
+            count = count + valuesToNumbers[card["value"]] //is it OK?
+        }
+    }
+}
+
+
+function cleanTable() {
+    //TODO clean: all the cards
+}
+
+
+function endGame() {
+    playerTurn = false;
+    inGame = false;
+}
+
+
+function gameSetup() {
+    indexInTable = [1, 1];
+    amount = [0, 0];
+    hitJS("Dealer");
+    hitJS("Player");
+    hitJS("Player");
+    document.getElementById("msg").innerText = "You may HIT or STAND"
+}
+
+
+function singleGame() {
+    //TODO get the amount of betting money of the user
+
+    getDeck();
+    shuffle();
+    cleanTable();
+    gameSetup();
+    inGame = true;
+    playerTurn = true;
 }
 
