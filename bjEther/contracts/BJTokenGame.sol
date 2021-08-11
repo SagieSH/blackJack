093036645083d73cf4ef0ebd8f206d6ff0da41d1
eytantirosh <eytantirosh@mail.tau.ac.il>
2021-08-11 18:48:01 +0300

    Update BJTokenGame.sol

--- bjEther/contracts/BJTokenGame.sol
+++ bjEther/contracts/BJTokenGame.sol
@@ -70,6 +70,10 @@ contract BJTokenGame {
         userToIndex["Dealer"] = 1;
     }
 
+    struct Card{
+        string value;
+        string suit;
+    }
     mapping(string => string)[52] deck;
 
 
@@ -84,28 +88,16 @@ contract BJTokenGame {
 
     function getDeck() {
 
-        for(let i = 0; i < suits.length; i++)
+        for(uint i = 0; i < suits.length; i++)
         {
-            for(let x = 0; x < values.length; x++)
+            for(uint x = 0; x < values.length; x++)
             {
-                let card = {"value": values[x], "suit": suits[i]};
+                card = {"value": values[x], "suit": suits[i]};
                 deck.push(card);
             }
         }
+        indexInDeck = 0;
     }
-    
-    function shuffle() {
-    // for 1000 turns
-    // switch the values of two random cards
-    for (let i = 0; i < 1000; i++)
-    {
-        let location1 = Math.floor((Math.random() * deck.length));
-        let location2 = Math.floor((Math.random() * deck.length));
-        let tmp = deck[location1];
 
-        deck[location1] = deck[location2];
-        deck[location2] = tmp;
-    }
-}
 
 }
