86d167d1fae6cbb81abaa6a72db93ade5462986e
SagieSH <sagie.shlfman@gmail.com>
2021-08-11 18:13:51 +0300

    11.08 18:13

--- bjEther/contracts/BJToken.sol
+++ bjEther/contracts/BJToken.sol
@@ -22,22 +22,7 @@ contract BJToken {
 
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
-
-    // constructor (uint256 _initialSupply) public {
-    //     balanceOf[msg.sender] = _initialSupply;
-    //     totalSupply = _initialSupply;
-    // }
-
-    // function transfer(address _to, uint256 _value) public returns (bool success) {
-    //     require(balanceOf[msg.sender] >= _value);
-
-    //     balanceOf[msg.sender] -= _value;
-    //     balanceOf[_to] += _value;
-
-    //     emit Transfer(msg.sender, _to, _value);
-
-    //     return true;
-    // }
+    
 
     function addAmount(address _to, uint256 _amount) public returns (bool success) {
         balanceOf[_to] += _amount;
@@ -50,25 +35,25 @@ contract BJToken {
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
+    // function approve(address _spender, uint256 _value) public returns (bool success) {
+    //     allowance[msg.sender][_spender] = _value;
 
-        emit Approval(msg.sender, _spender, _value);
+    //     emit Approval(msg.sender, _spender, _value);
 
-        return true;
-    }
+    //     return true;
+    // }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
+    // function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    //     require(_value <= balanceOf[_from]);
+    //     require(_value <= allowance[_from][msg.sender]);
 
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
+    //     balanceOf[_from] -= _value;
+    //     balanceOf[_to] += _value;
 
-        allowance[_from][msg.sender] -= _value;
+    //     allowance[_from][msg.sender] -= _value;
 
-        emit Transfer(_from, _to, _value);
+    //     emit Transfer(_from, _to, _value);
 
-        return true;
-    }
+    //     return true;
+    // }
 }
--- bjEther/contracts/BJTokenGame.sol
+++ bjEther/contracts/BJTokenGame.sol
@@ -14,6 +14,7 @@ contract BJTokenGame {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
+        initializeMappings();
     }
 
     function multiply(uint x, uint y) internal pure returns (uint z) {
@@ -40,7 +41,48 @@ contract BJTokenGame {
         emit BalanceOf(msg.sender, tokenContract.balanceOf(msg.sender));
     }
 
-    //-------------------------------- Game Functions ----------------------------------------------
+
+    //-------------------------------- Game Logic ----------------------------------------------
+
+    string suits[4] = ["\u2660", "\u2662", "\u2667", "\u2665"];
+    string values[13] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    mapping(string => uint) valuesToNumbers;
+    mapping(string => uint) userToIndex;
+
+    function initializeMappings() {
+
+        valuesToNumbers["A"] = 11;
+        valuesToNumbers["2"] = 2;
+        valuesToNumbers["3"] = 3;
+        valuesToNumbers["4"] = 4;
+        valuesToNumbers["5"] = 5;
+        valuesToNumbers["6"] = 6;
+        valuesToNumbers["7"] = 7;
+        valuesToNumbers["8"] = 8;
+        valuesToNumbers["9"] = 9;
+        valuesToNumbers["10"] = 10;
+        valuesToNumbers["J"] = 10;
+        valuesToNumbers["Q"] = 10;
+        valuesToNumbers["K"] = 10;
+
+        userToIndex["Player"] = 0;
+        userToIndex["Dealer"] = 1;
+    }
+
+    mapping(string => string) deck[52];
+
+
+    // let deck = new Array();
+    // let playerTurn = false;     // indicates whether the player can hit or stand
+    // let inGame = false;         // indicates whether the player is currently in game
+    // let BUSTLIMIT = 22;
+    // let DEALLIMIT = 17;
+
+    // let indexInTable;
+    // let amount;
+    // let countAces;
+    // let balance;
 
 
 }
